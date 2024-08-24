c197caa112805e96bfb78ce86f092fef87d16c5c
YHOneBox <102507655+YHOneBox@users.noreply.github.com>
2024-08-24 14:28:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('openGame').addEventListener('click', function() {
+    document.getElementById('gameContainer').style.display = 'block';
+});
+
+document.getElementById('closeGame').addEventListener('click', function() {
+    document.getElementById('gameContainer').style.display = 'none';
+});
